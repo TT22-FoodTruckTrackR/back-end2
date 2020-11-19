642bb542f6101a0cfc5ddffe5078de460301467c
Suzanne Cabral <suzannecabral@live.com>
2020-11-19 10:58:51 -0800

    Added seed data

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -8,8 +8,52 @@ exports.seed = function(knex) {
         {
           username:"bob",
           password:"bob",
-          email:"bob@bobsburgers.com"
-      },
+          email:"bob@bobsburgers.com",
+          isOperator:1,
+          location:1,
+        },
+        {
+          username:"linda",
+          password:"linda",
+          email:"linda@bobsburgers.com",
+          isOperator:1,
+          location:2,
+        },
+        {
+          username:"tina",
+          password:"tina",
+          email:"tina@bobsburgers.com",
+          isOperator:0,
+          location:1,
+        },
+        {
+          username:"louise",
+          password:"louise",
+          email:"louise@bobsburgers.com",
+          isOperator:0,
+          location:2,
+        },
+        {
+          username:"gene",
+          password:"gene",
+          email:"gene@bobsburgers.com",
+          isOperator:0,
+          location:1,
+        },
+        {
+          username:"jimmy",
+          password:"jimmy",
+          email:"jimmy@pestopizzeria.com",
+          isOperator:1,
+          location:2,
+        },
+        {
+          username:"tommy",
+          password:"tommy",
+          email:"tommy@email.com",
+          isOperator:1,
+          location:3,
+        },
       ]);
     });
 };
