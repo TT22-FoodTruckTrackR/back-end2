4c7cf80727ecbd9ff5ea4739c33a2c349679ebc6
Suzanne Cabral <suzannecabral@live.com>
2020-11-19 18:01:54 -0800

    delete trucks endpoint added

--- api/trucks/trucksRouter.js
+++ api/trucks/trucksRouter.js
@@ -62,7 +62,86 @@ router.get('/:id', restricted, (req,res)=>{
  
 
 
+// POST /api/trucks/
+//---------------------------------
 
+/*
+  {
+    "ownerID":14,
+    
+    "name":"Cleo's Cupcakes",
+    "type":"Dessert",
+    "photoUrl":"https://www.bakemag.com/ext/resources/images/w/i/c/k/e/k/c/u/u/r/2018/WickedGoodCupcakes_Truck.jpg",
+    "location":2
+  }
+*/
+
+router.post('/', (req, res, next)=>{
+  const newTruck = req.body;
+
+  //next: add operator_id as second arg to addNewTruck
+  //functionality thru db method
+
+  Trucks.addNewTruck(newTruck)
+    .then(truck => {
+      res.status(201).json(truck);
+      next();
+    })
+    .catch(err=>{
+      console.log(err);
+      res.status(500).json({
+        message:'Server error retrieving creating new user',
+        errorName:err.name, 
+        errorMessage:err.message,
+      });
+    });
+});
+
+
+// PUT /api/trucks/
+//---------------------------------
+/*
+  {
+    "ownerID":14,
+    
+    "name":"Cleo's Cupcakes",
+    "type":"Dessert",
+    "photoUrl":"https://www.bakemag.com/ext/resources/images/w/i/c/k/e/k/c/u/u/r/2018/WickedGoodCupcakes_Truck.jpg",
+    "location":2
+  }
+*/
+
+// router.put('/:id', (req,res)=>{
+//   const {id} = req.params;
+
+//   Trucks.updateTruck(id, req.body)
+
+// });
+
+
+
+
+// DELETE /api/trucks/:id
+//---------------------------------
+
+//needs checkOwned validation
+
+router.delete('/:id', restricted, (req,res)=>{
+  const id = req.params.id;
+
+  Trucks.getTruckById(id)
+  .then(truck => {
+    res.status(200).json({message:'Deleted successfully', truck:truck});
+  })
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json({
+      message:'Server error retrieving truck info',
+      errorName:err.name,
+      errorMessage:err.message
+    });
+  })
+});
 
 //export
 module.exports = router;
