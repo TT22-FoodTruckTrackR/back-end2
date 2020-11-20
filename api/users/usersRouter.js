4a129f1b237e550f6013692fbc2ffc3884b36e7f
Suzanne Cabral <suzannecabral@live.com>
2020-11-19 18:51:16 -0800

    Added restricted middleware to trucks routes

---
+++ api/users/usersRouter.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const Trucks = require('./trucksModel');
+
+const restricted = require('../auth/restricted');
+
+
+
+//new router
+const router = express.Router();
+
+
+// //GET /api/users
+//---------------------------------
+router.get('/users', (req, res, next)=>{
+  Users.getUsers()
+    .then(users => {
+      res.status(200).json(users);
+      next();
+    })
+    .catch(err=>{
+      console.log(err);
+      res.status(500).json({
+        message:'Server error retrieving userlist',
+        errorName:err.name,
+        errorMessage:err.message,
+      });
+    })
+});
+
+
+//export
+module.exports = router;
